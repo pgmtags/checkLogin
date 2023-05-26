9c60e56884201f7df3f6e1a6100f36c22066c757
Andrii <zinukandrij@gmail.com>
2023-05-26 19:53:31 +0300

    added js

---
+++ script.js
@@ -0,0 +1,22 @@
+// Заранее заданные значения логина и пароля
+const correctUsername = "admin";
+const correctPassword = "password123";
+
+// Функция для проверки логина и пароля
+function checkLogin(username, password) {
+  if (username === correctUsername && password === correctPassword) {
+    console.log("Login successful!");
+    alert("Login successful!");
+  } else {
+    console.log("Error login.");
+    alert("Error login.");
+  }
+}
+
+// Функция для обработки события нажатия кнопки Login
+function validateLogin() {
+  const enteredUsername = document.getElementById("username").value;
+  const enteredPassword = document.getElementById("password").value;
+
+  checkLogin(enteredUsername, enteredPassword);
+}
